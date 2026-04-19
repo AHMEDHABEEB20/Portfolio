3619e394ffd212bb83fb13fb371bf02fc92094e2
AhmedHabeeb <ahmedhabeeb7777777@gmail.com>
2026-04-19 19:36:53 +0300

    display navbar in mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={`${inter.variable} ${spaceMono.variable} font-sans antialiased`}>
+      <body className={`${inter.variable} ${spaceMono.variable} font-sans antialiased overflow-x-hidden`}>
         {children}
         <Analytics />
       </body>
